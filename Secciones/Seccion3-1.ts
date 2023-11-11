889c9705782a1acfedf20e0cdf3e1bc5b6ac3d3d
David Vilchis <davidvilchis63@gmail.com>
2023-11-11 14:51:15 -0600

    Guardando seccion 3, tipos de datos

---
+++ Secciones/Seccion3-1.ts
@@ -0,0 +1,28 @@
+
+//Tipos de datos
+
+//Strings - cadenas de texto
+let firstName: string = "Fernando";
+let lastName: string = "Herrera";
+
+console.log(firstName, lastName);
+
+let fullName: string = firstName + " " + lastName;
+
+console.log(fullName);
+
+// Numeros
+let number1: number = 10;
+let number2: number = 20;
+
+console.log(number1 - number2);
+
+//Booleanos - verdadero o falso - 1 o 0
+
+let isActive: boolean = true;
+let isRunning: boolean = true;
+let hasKids: boolean = false;
+
+console.log("Esta activo:", isActive);
+console.log("Esta corriendo:", !isRunning);
+console.log("Esta activo:", hasKids);
