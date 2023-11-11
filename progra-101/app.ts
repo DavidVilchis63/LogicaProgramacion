48f1677ffb16253878556ec7c234f1991273da9d
David Vilchis <davidvilchis63@gmail.com>
2023-11-11 12:48:56 -0600

    Programacion secuencial

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,4 +1,16 @@
 
-console.log('Hola Mundo 2');
+//console.log('Hola Mundo 2');
 
+let numbersOfLines = 1;
 
+console.log('Linea #', numbersOfLines);
+numbersOfLines = numbersOfLines + 1;
+
+console.log('Linea #', numbersOfLines);
+numbersOfLines = numbersOfLines + 1;
+
+console.log('Linea #', numbersOfLines);
+numbersOfLines = numbersOfLines + 1;
+
+console.log('Linea #', numbersOfLines);
+numbersOfLines = numbersOfLines + 1;
