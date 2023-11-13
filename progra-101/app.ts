c3c875aa8953cb6991d1180e5b29d8c5cb141e0e
David Vilchis <davidvilchis63@gmail.com>
2023-11-13 15:55:20 -0600

    Guardar seccion,3-3

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -2,16 +2,3 @@
 
 console.log("Inicio de programa");
 
-let grade1 = 70;
-
-if ( grade1 >= 90){
-    console.log("Nota : A");    
-}else if( grade1 >= 80){
-    console.log("Nota : B");
-}else if( grade1 >= 70){
-    console.log("Nota : C");
-}else if( grade1 >= 60){
-    console.log("Nota : D");
-}else{
-    console.log("Nota : F");
-}
