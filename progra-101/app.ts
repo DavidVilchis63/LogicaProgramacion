4047c9459b8d228ca457d970e02eba0bb56ef7e4
David Vilchis <davidvilchis63@gmail.com>
2023-11-13 15:27:19 -0600

    Estructura de if

---
+++ progra-101/app.ts
@@ -0,0 +1,16 @@
+
+
+console.log("Inicio de programa");
+
+let isTired: boolean = false;
+
+if( isTired){ 
+
+    console.log("Tomar cafe");
+} else {
+    console.log("Tomar agua")
+}
+
+
+
+console.log("Fin de programa");
