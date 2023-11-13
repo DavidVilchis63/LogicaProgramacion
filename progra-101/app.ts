df993fd72ceeaeca22cc134a4c2f126af17a2dfb
David Vilchis <davidvilchis63@gmail.com>
2023-11-13 15:42:07 -0600

    Guardar seccion

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,24 +1,3 @@
 
 
 console.log("Inicio de programa");
-
-let isTired:boolean = false;
-
-// if( isTired){ 
-
-//     console.log("Tomar cafe");
-// } else {
-//     console.log("Tomar agua")
-// }
-
-let grade:number = 40;
-
-if (grade >= 60){
-    console.log("Calificacion aprobatoria");
-} else if (grade > 50) {
-    console.log("Estudiar mas");
-} else {
-    console.log("Calificacion reprobatoria");
-}   
-
-console.log("Fin de programa");
