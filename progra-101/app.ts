f3e77ee6caef9a08d964c931b2d1002d159ce9bc
David Vilchis <davidvilchis63@gmail.com>
2023-11-13 22:28:22 -0600

    Exporta e importar funcion

---
+++ progra-101/app.ts
@@ -0,0 +1,6 @@
+
+import { addTwoNumbers } from './helpers/math-helpers';
+
+let total1:number = addTwoNumbers(20,63);
+
+console.log("La suma es:" ,total1);
---
+++ progra-101/helpers/math-helpers.ts
@@ -0,0 +1,6 @@
+//Suma
+
+export function addTwoNumbers (num1:number, num2:number){
+    let resultado = num1 + num2;
+    return resultado;
+}
