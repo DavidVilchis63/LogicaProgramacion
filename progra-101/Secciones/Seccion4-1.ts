5c66e4cd9803e2b7fe276f0a17adf1d39b1b24e8
David Vilchis <davidvilchis63@gmail.com>
2023-11-13 22:14:55 -0600

    Guardar seccion 4-1

---
+++ progra-101/Secciones/Seccion4-1.ts
@@ -0,0 +1,17 @@
+
+// function greet( name:string) {
+//     console.log("Hola", name);
+// }
+
+// greet("David");
+
+//Ejemplo funcion
+
+function addTwoNumbers (num1:number, num2:number){
+    let resultado = num1 + num2;
+    return resultado;
+}
+
+let total1:number = addTwoNumbers(20,63);
+
+console.log("La suma es:" ,total1);
