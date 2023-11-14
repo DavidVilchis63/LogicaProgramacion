907d2ee5335be1b1f1a99751c896dbe58296561d
David Vilchis <davidvilchis63@gmail.com>
2023-11-13 20:22:38 -0600

    Estructura Switch

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,4 +1,21 @@
 
+let weekDay: number = 1; ////lunes =1
 
-console.log("Inicio de programa");
+switch(weekDay){
+    case 1:
+        console.log("Es lunes");
+        break;
 
+    case 2:
+        console.log("Es martes");
+        break;
+
+    case 3:
+        console.log("Es miercoles");
+        break;
+
+    default:
+        console.log("Condicion por default")
+
+
+}
