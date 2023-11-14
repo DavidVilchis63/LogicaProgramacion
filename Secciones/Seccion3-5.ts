fff871d899b4a1c19af3f070b815c23d0924106f
David Vilchis <davidvilchis63@gmail.com>
2023-11-13 20:44:45 -0600

    Guardar seccion 3-4 y 3-5

---
+++ Secciones/Seccion3-5.ts
@@ -0,0 +1,25 @@
+//Ciclo While
+
+// let gasTank: number = 10;
+
+// while (gasTank > 0){
+//     console.log("Gasolina restanta: ", gasTank);
+    
+//     gasTank --;
+
+// }
+
+// console.log("Sin combustible");
+
+//Ciclo do while
+
+let gasTank: number = 10;
+
+do {
+    console.log("Gasolina restanta: ", gasTank);
+    
+    gasTank --;
+
+} while ( gasTank > 0);
+
+console.log("Sin combustible");
