884a1b18c3a4ca4917da03bac8940644355e608c
David Vilchis <davidvilchis63@gmail.com>
2023-11-15 21:06:39 -0600

    Ejercicio funciones

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,6 +1,18 @@
 
 import { addTwoNumbers } from './helpers/math-helpers';
+import { substractTwoNumbers } from './helpers/math-helpers';
+import { divideTwoNumbers } from './helpers/math-helpers';
+import { timesTwoNumbers } from './helpers/math-helpers';
 
-let total1:number = addTwoNumbers(20,63);
+let numero1: number = 7;
+let numero2: number = 7;
+
+let total1:number = addTwoNumbers(numero1, numero2);
+let total2:number = substractTwoNumbers(numero1, numero2);
+let total3:number = timesTwoNumbers(numero1, numero2);
+let total4:number = divideTwoNumbers(numero1, numero2);
 
 console.log("La suma es:" ,total1);
+console.log("La resta es:" ,total2);
+console.log("La multiplicacion es:" ,total3);
+console.log("La division es:" ,total4);
--- progra-101/helpers/math-helpers.ts
+++ progra-101/helpers/math-helpers.ts
@@ -7,9 +7,21 @@ export function addTwoNumbers (num1:number, num2:number){
 
 // substract -
 
+export function substractTwoNumbers (num1:number, num2:number){
+    let resultado = num1 - num2;
+    return resultado;
+}
 
 // Times *
 
-
+export function timesTwoNumbers (num1:number, num2:number){
+    let resultado = num1 * num2;
+    return resultado;
+}
 
 // Divide / 
+
+export function divideTwoNumbers (num1:number, num2:number){
+    let resultado = num1 / num2;
+    return resultado;
+}
