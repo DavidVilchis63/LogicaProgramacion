bf4a8ed46c6c2f14fc938503daa2c12b3e68fbad
David Vilchis <davidvilchis63@gmail.com>
2023-11-15 21:30:09 -0600

    Funcion de multiplicacion

---
+++ progra-101/helpers/print-multiplication.ts
@@ -0,0 +1,10 @@
+
+export function printMultiplication ( base: number, limite: number= 10 ){
+        
+    for (let i: number= 1; i<= limite ; i++){
+
+        console.log(base, "X", limite , "=" ,base*limite);
+
+    }
+
+}
