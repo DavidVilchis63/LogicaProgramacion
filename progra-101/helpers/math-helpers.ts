166d972fd8479807ebbabb66b09ff8026c26e29c
David Vilchis <davidvilchis63@gmail.com>
2023-11-15 20:50:24 -0600

    Agregando comentarios

--- progra-101/helpers/math-helpers.ts
+++ progra-101/helpers/math-helpers.ts
@@ -1,6 +1,15 @@
-//Suma
+//add +
 
 export function addTwoNumbers (num1:number, num2:number){
     let resultado = num1 + num2;
     return resultado;
 }
+
+// substract -
+
+
+// Times *
+
+
+
+// Divide / 
