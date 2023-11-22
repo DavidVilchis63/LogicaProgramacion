cac8a6a4a485f5372388c519a0eb59796be856b9
David Vilchis <davidvilchis63@gmail.com>
2023-11-21 21:09:03 -0600

    Objetos literarios, II

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -13,10 +13,35 @@ let person = {
 
 }
 
-// Imprimir todo el contenido del objeto
-console.log(person);
+// // Imprimir todo el contenido del objeto
+// console.log(person);
 
-// Imprime una propiedad especifica del objeto
-console.log(person.age);
+// // Imprime una propiedad especifica del objeto
+// console.log(person.age);
 
-person.toString();
+// person.toString();
+
+let car = {
+    model: "Focus",
+    brand: "Ford",
+    year: 2002,
+    color: "White"
+}
+
+let smartTV ={
+    brand: "LG",
+    model: 2023,
+    color: "Black",
+    resolution: "4K"
+}
+
+let youTubeVideo = {
+    year: 2022,
+    user: "Udemy",
+    time: 3600,
+    views: 2153
+}
+
+console.log(car);
+console.log(smartTV);
+console.log(youTubeVideo);
