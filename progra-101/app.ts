123b739504ebce299c859be702e6634057fdd61d
David Vilchis <davidvilchis63@gmail.com>
2023-11-21 20:52:51 -0600

    Objetos literarios

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,1 +1,22 @@
-console.log("Hola mundo");
+
+// Objetos literales
+
+let person = {
+    name: "Melissa",
+    age: 30,
+    isActive: true,
+    hobbies: ["soccer", "music"],
+    toString () {
+        let objectoString = this.name + " " + this.age; 
+        console.log( objectoString );
+    }
+
+}
+
+// Imprimir todo el contenido del objeto
+console.log(person);
+
+// Imprime una propiedad especifica del objeto
+console.log(person.age);
+
+person.toString();
