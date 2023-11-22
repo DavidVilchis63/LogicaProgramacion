42e6594ca4a1fab8dfc1da3a2e2f3be40beafab0
David Vilchis <davidvilchis63@gmail.com>
2023-11-21 21:27:16 -0600

    Arreglos

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,2 +1,35 @@
 
-console.log("Hola mundo");
+// Arreglos de objetos
+
+interface Person {
+    name: string;
+    age: number;
+    isActive: boolean;
+}
+
+
+
+let fernando: Person = {
+    name: "Fernando",
+    age: 25,
+    isActive: true
+};
+
+let luisa: Person = {
+    name: "Luisa",
+    age: 32,
+    isActive: false
+};
+
+let maggie: Person = {
+    name: "Maggie",
+    age: 26,
+    isActive: false
+};
+
+let people: Person[] = [fernando , luisa, maggie];
+
+for ( let i = 0; i < people.length; i++) {
+    let person = people[i];
+    console.log(person.name + " " + person.age );
+}
