14df819952729126587db1323924edb09a98ae00
David Vilchis <davidvilchis63@gmail.com>
2023-11-21 20:27:15 -0600

    Ejercicio arreglo II

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -2,18 +2,30 @@
 //Arreglos
 //Posicion base 0 [0,1,2,3]
 
-let people =['Fernando', "Melissa", "Eduardo"];
+ let people =['Fernando', "Melissa", "Eduardo"];
 
-console.log(people[1]);
+// console.log(people[1]);
 
 
 // Ejercicio 
 
 let flowers: string[] = ["Girasol", "Rosa", "Lirio", "RosaBlanca"];
 
-console.log(flowers.length);
+// console.log(flowers.length);
 
-for(let i= 0; i < flowers.length; i++){
+// for(let i= 0; i < flowers.length; i++){
     
-    console.log(flowers[i]);
+//     console.log(flowers[i]);
+// }
+
+// Ejercicio 2
+
+let salaries: number[] = [10000, 12000, 15000];
+
+console.log(people[1]);
+console.log(salaries[1]);
+
+for (let i = 0; i < people.length ; i++){
+
+    console.log("El empleado:", people[i], "tiene un salario de:" , salaries[i]);
 }
