7e8e5767e599ac69a5a0845849e105ccff7b5cbd
David Vilchis <davidvilchis63@gmail.com>
2024-02-05 14:58:45 -0600

    Ejercicio 1, respaldo

---
+++ progra-101/Ejercicios/Ejercicio1.ts
@@ -0,0 +1,8 @@
+
+//Ejercicio 1
+//Convertir libras a kilogramos
+
+let pounds = 2;
+let kilogramos = pounds * 0.453592;
+
+console.log(pounds, "libras es igual a:", kilogramos, "kg");
--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,8 +1,2 @@
 
-//Ejercicio 1
-//Convertir libras a kilogramos
-
-let pounds = 2;
-let kilogramos = pounds * 0.453592;
-
-console.log(pounds, "libras es igual a:", kilogramos, "kg");
+console.log("Hola")
