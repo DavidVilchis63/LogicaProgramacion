101fc16ad7affc59c5e08f31a5e856c006677d8c
David Vilchis <davidvilchis63@gmail.com>
2024-02-05 15:02:33 -0600

    Ejercicio 2, respaldo

---
+++ progra-101/Ejercicios/Ejercicio2.ts
@@ -0,0 +1,8 @@
+
+//Ejercicio 2
+//Convertir de kilómetros a millas, sabiendo que: (1 kilómetro es igual a 0.62 millas )
+
+let km = 10;
+let mll = km * 0.62;
+
+console.log(km, "kilometros es igual a:", mll, "millas");
