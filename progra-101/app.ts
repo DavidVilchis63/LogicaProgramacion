60bcf4b25d62bc0e33975224d1327bbf056e74f1
David Vilchis <davidvilchis63@gmail.com>
2024-02-07 09:03:42 -0600

    Ejercicio 3

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,2 +1,10 @@
 
-console.log("HOla")
+//Ejercicio 3
+// Calcule el área de un triangulo rectángulo que: 
+//Base 10 y altura 5 
+
+let base = 10;
+let altura = 5;
+
+let area = (base * altura)/2;
+console.log("El area es:", area);
