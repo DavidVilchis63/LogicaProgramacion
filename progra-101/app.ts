e26ea774c1369a831bc9b16cad99413265b7b7f9
David Vilchis <davidvilchis63@gmail.com>
2024-03-04 11:55:40 -0600

    Ejercicio 8

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,2 +1,34 @@
 
-console.log("Hola")
+// Ejercicio 8
+
+// Crear una función que reciba como argumento un arreglo, la función debe de retornar el nombre que tenga más letras del arreglo, y debe de trabajar así: 
+
+// Tip: 
+// let hero = 'Strider'; 
+// hero.length; // 7
+
+
+function largestNameOfArray (nameArray: string[]) {
+
+    let nombreComparar = nameArray[0];
+
+    for (let i= 1; i < nameArray.length; i++){
+
+        if(nameArray[i].length > nombreComparar.length){
+            nombreComparar = nameArray[i];
+
+        }
+
+    }
+
+    return nombreComparar;
+
+}
+
+
+
+
+let heroes = ['Deadpool', 'Ciclope', 'Magneto', 'Profesor Charles Xavier']; 
+
+let hero = largestNameOfArray(heroes); 
+console.log( hero ); // Profesor Charles Xavier 
