afb8d7639801d485cfb8048ee8294b5f6453123d
David Vilchis <davidvilchis63@gmail.com>
2024-03-06 18:39:50 -0600

    Inicio de pagina de juego, se colocan botones

--- hanging/src/App.tsx
+++ hanging/src/App.tsx
@@ -1,13 +1,33 @@
-import './App.css'
+import './App.css';
+import {letters} from './helpers/letters';
 
 function App() {
 
   return (
     <div className='App'>
-      <h1> Hola mundo</h1>
+
+          {/* Imagenes */}
+          <h3>Imagen del juego</h3>
+
+          {/* Palabra oculta */}
+          <h3>_ _ _ _ _ _ _ _ _ _</h3>
+
+          {/* Contador de intentos */}
+          <h3> Intentos : 0</h3>
+
+          {/* Botones */}
+          {
+            letters.map( (letter) => (
+              <button 
+               key= { letter }>
+                { letter }
+              </button>
+            ))
+          }
+
     </div>
-  )
+  );
    
-}
+};
 
 export default App
